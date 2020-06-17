b5c35ed1da2521aa4864574e57aa228b192bd432
Lucas <lluuccaassfm@gmail.com>
2020-06-16 21:02:24 -0300

    Faz movimentação do jogador

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pygame
 pygame.init()
 
 # Cria a tela
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800, 600))
 
 # Adciona Titulo e icone
 pygame.display.set_caption("Jogo de Navinha")
@@ -13,21 +13,35 @@ pygame.display.set_icon(icon)
 
 # imagm e posicao do jogador
 imgJogador = pygame.image.load("jogador.png")
-jogadorX   = 370
-jogadorY   = 480
+jogadorX = 370
+jogadorY = 480
+jogadorX_change = 0
 
-def jogador():
-    screen.blit(imgJogador, (jogadorX, jogadorY))
+
+def jogador(x, y):
+    screen.blit(imgJogador, (x, y))
+
 
 # Loop do jogo
 running = True
 while running:
     # cor da tela
-    screen.fill((0,0,0))
+    screen.fill((0, 0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    jogador()
+        # verifica se tecla pressionada é esquerda ou direita
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                jogadorX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                jogadorX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                jogadorX_change = 0
+
+    jogadorX += jogadorX_change
+    jogador(jogadorX, jogadorY)
     pygame.display.update()
